136fa81b3136260cdf85e87cbfae77de6654a84a
inugent <>
2010-01-27 14:29:08 +0000

    This commit was manufactured by cvs2svn to create branch 'KinTau-1-ambiguity'.
    Sprout from master 2010-01-27 14:29:07 UTC perchall 'first reviewed version'
    Delete:
        .admin/developers
        check_this_out.txt
        src/CommonTools/PrimVtxSelector/BuildFile
        src/CommonTools/PrimVtxSelector/interface/PrimVtxSelector.h
        src/CommonTools/PrimVtxSelector/primvtxselector_cfg.py
        src/CommonTools/PrimVtxSelector/python/PrimVtxSelector_cfi.py
        src/CommonTools/PrimVtxSelector/src/PrimVtxSelector.cc
        src/DataFormats/KinematicFit/BuildFile
        src/DataFormats/KinematicFit/interface/PFTauFwd.h
        src/DataFormats/KinematicFit/interface/SelectedKinematicDecay.h
        src/DataFormats/KinematicFit/interface/SelectedKinematicParticle.h
        src/DataFormats/KinematicFit/interface/TrackFwd.h
        src/DataFormats/KinematicFit/src/SelectedKinematicDecay.cc
        src/DataFormats/KinematicFit/src/SelectedKinematicParticle.cc
        src/DataFormats/KinematicFit/src/classes.h
        src/DataFormats/KinematicFit/src/classes_def.xml

--- src/CommonTools/PrimVtxSelector/python/PrimVtxSelector_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-PrimVtxSelector = cms.EDFilter("PrimVtxSelector",
-	primVtx = cms.InputTag("offlinePrimaryVertices"),#offlinePrimaryVerticesFromCTFTrack
-	minTracks = cms.untracked.int32(3),
-	maxChi2ndf = cms.untracked.double(10.0),
-	verbosity = cms.untracked.int32(1)
-)
