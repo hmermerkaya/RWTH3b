225f65ccf3860b296924d3f69613fba8bc90f84b
sauerlan <>
2010-06-29 19:13:00 +0000

    removal of dangerous static globaltag in cfi python files

--- src/RecoTauTag/KinematicTau/python/ThreeProngInputSelector_cfi.py
+++ src/RecoTauTag/KinematicTau/python/ThreeProngInputSelector_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.MagneticField_cff import *
 from Configuration.StandardSequences.Geometry_cff import *
-from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import * #https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions#Global_Tags_for_Monte_Carlo_Prod
-GlobalTag.globaltag = 'MC_31X_V3::All'
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import *
 
