a5f4557136ef5f76647c484f8d6b82a8913f9fc5
perchall <>
2011-12-01 10:18:59 +0000

    switch to hps taus

--- src/RecoTauTag/KinematicTau/python/InputTrackSelector_cfi.py
+++ src/RecoTauTag/KinematicTau/python/InputTrackSelector_cfi.py
@@ -1,9 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-#pfTau = 'shrinkingCone'		#Signal cone: dR = 5/Et, Iso Cone = 0.5, 0.07 < dR < 0.15; where Et is the tau tranvserse energy
+#pfTau = 'shrinkingCone'		#Signal cone: dR = 5/Et, Iso Cone = 0.5, 0.07 < dR < 0.15; where Et is the tau tranvserse energy (still up-to-date?)
+pfTau = 'hps'
 #pfTau = 'fixedCone'			#Signal cone: dR = 0.07, Iso Cone = 0.5
-pfTau = 'fixedConeHighEff'		#Signal cone: dR = 0.15, Iso Cone = 0.5
-#pfTau = 'hps'
+#pfTau = 'fixedConeHighEff'		#Signal cone: dR = 0.15, Iso Cone = 0.5
 
 
 InputTrackSelector = cms.EDFilter("InputTrackSelector",#creates PFTauRefVector and collection of vector<reco::TrackRefVector> for each tau cand
