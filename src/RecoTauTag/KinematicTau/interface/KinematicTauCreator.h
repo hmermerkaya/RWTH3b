840527d82d2b2536449ddb83bf5a6868f600f0d1
perchall <>
2012-01-25 13:03:12 +0000

    small cleanup

--- src/RecoTauTag/KinematicTau/interface/KinematicTauCreator.h
+++ src/RecoTauTag/KinematicTau/interface/KinematicTauCreator.h
@@ -11,13 +11,13 @@
  * @date 2010
  */
 //
-// $Id: KinematicTauCreator.h,v 1.18 2010/08/13 14:22:54 perchall Exp $
+// $Id: KinematicTauCreator.h,v 1.19 2010/11/25 09:21:27 perchall Exp $
 //
 //
 //
 // Original Author:  Lars Perchalla, Philip Sauerland
 //         Created:  Tue Jan 12 15:13:30 CET 2010
-// $Id: KinematicTauCreator.h,v 1.18 2010/08/13 14:22:54 perchall Exp $
+// $Id: KinematicTauCreator.h,v 1.19 2010/11/25 09:21:27 perchall Exp $
 //
 //
 
@@ -31,81 +31,80 @@
 #include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
 
 
-class KinematicTauCreator
-{
+class KinematicTauCreator {
 public:
-	/**
-	 default constructor
-	 */
+    /**
+     default constructor
+     */
     KinematicTauCreator(const TransientTrackBuilder & transTrackBuilder);
-	/**
-	 constructor with user defined fit parameters
-	 */
+    /**
+     constructor with user defined fit parameters
+     */
     KinematicTauCreator(const TransientTrackBuilder & transTrackBuilder, const edm::ParameterSet& cfg);
     virtual ~KinematicTauCreator();
 
-	/**
-	 main function of the KinematicTau algorithm. It will call the Fitter and provide the fitted results. It returns true if the fit was successfull.
-	 */
+    /**
+     main function of the KinematicTau algorithm. It will call the Fitter and provide the fitted results. It returns true if the fit was successfull.
+     */
     virtual int create(const reco::Vertex& primaryVertex, const std::vector<reco::TrackRef>& inputTracks) = 0;
 
-	/**
-	 visible tau constructed from refitted tracks excluding neutrals (the stored primary vertex might be rotated)
-	 */
+    /**
+     visible tau constructed from refitted tracks excluding neutrals (the stored primary vertex might be rotated)
+     */
     reco::PFTau getPFTau() const;
-	/**
-	 the full tau constructed from refitted tracks including neutrals (the stored primary vertex might be rotated)
-	 */
-	reco::PFTau getKinematicTau() const;
-	/**
-	 returns vector of all refitted charged particles involved in the fit
-	 */
+    /**
+     the full tau constructed from refitted tracks including neutrals (the stored primary vertex might be rotated)
+     */
+    reco::PFTau getKinematicTau() const;
+    /**
+     returns vector of all refitted charged particles involved in the fit
+     */
     std::vector<math::XYZTLorentzVector> getRefittedChargedDaughters() const;
-	/**
-	 returns vector of all refitted neutral particles involved in the fit
-	 */
+    /**
+     returns vector of all refitted neutral particles involved in the fit
+     */
     std::vector<math::XYZTLorentzVector> getRefittedNeutralDaughters() const;
 
-	/**
-	 ref to original tracks used by the fit
-	 */
+    /**
+     ref to original tracks used by the fit
+     */
     std::vector<reco::TrackRef> getSelectedTracks() const;
-	
-	/**
-	 for debug issues only
-	 */
+    
+    /**
+     for debug issues only
+     */
     RefCountedKinematicTree getKinematicTree() const;
-	
-	/**
-	 for debug issues only
-	 */
-	KinematicConstrainedVertexFitter * getFitter() const {return kcvFitter_;}
-	
-	/**
-	 If the primary vertex was modified by a call of create() this function returns a fake vertex at the modified position with the initial errors. 
-	 Otherwise an invalid reco::Vertex is returned.
-	 */
-	reco::Vertex getModifiedPrimaryVertex() const;
-	
-	float chi2() const ;
-	/**
-	 int depends on specific decay.
-	 */
-	virtual int ndf() const = 0;
-	
+    
+    /**
+     for debug issues only
+     */
+    KinematicConstrainedVertexFitter * getFitter() const {return kcvFitter_;}
+    
+    /**
+     If the primary vertex was modified by a call of create() this function returns a fake vertex at the modified position with the initial errors. 
+     Otherwise an invalid reco::Vertex is returned.
+     */
+    reco::Vertex getModifiedPrimaryVertex() const;
+    
+    float chi2() const ;
+    /**
+     int depends on specific decay.
+     */
+    virtual int ndf() const = 0;
+    
 protected:
-	/**
-	 each call of create() will update these members
-	 */
+    /**
+     each call of create() will update these members
+     */
     KinematicConstrainedVertexFitter *kcvFitter_;
     RefCountedKinematicTree kinTree_;
-	/**
-	 ref to original tracks used by the fit
-	 */
+    /**
+     ref to original tracks used by the fit
+     */
     std::vector<reco::TrackRef> selectedTracks_;
-	TransientTrackBuilder transTrackBuilder_;
-	/**
-	 Fake vertex. The primary vertex can be rotated within its errors around the secondary vertex. Errors are copied from the original vertex as they have not been modified.
-	 */
-	reco::Vertex modifiedPV_;
+    TransientTrackBuilder transTrackBuilder_;
+    /**
+     Fake vertex. The primary vertex can be rotated within its errors around the secondary vertex. Errors are copied from the original vertex as they have not been modified.
+     */
+    reco::Vertex modifiedPV_;
 };
