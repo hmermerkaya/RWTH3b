28ab06bdbf6cbc885acc93511f2f680fb60bea36
sauerlan <>
2012-01-31 14:01:05 +0000

    globaltag = 'GR_P_V27::All'

--- src/DataFormats/KinematicFit/src/classes.h
+++ src/DataFormats/KinematicFit/src/classes.h
@@ -2,6 +2,7 @@
 #include "DataFormats/KinematicFit/interface/SelectedKinematicParticle.h"
 #include "DataFormats/KinematicFit/interface/SelectedKinematicDecay.h"
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/TauReco/interface/PFTau.h"
 
 #include <vector>
@@ -17,6 +18,11 @@ namespace {
 		std::vector<reco::TrackRefVector> vvr2;
 		edm::Wrapper<std::vector<reco::TrackRefVector> > wvvr2;
 		edm::Wrapper<reco::PFTauRef> wvr3;
+
+        std::vector<std::vector< reco::TrackRef > > vvr1;        
+        edm::Wrapper<std::vector<std::vector< reco::TrackRef > > > wvvr1;
+        std::vector<std::vector<std::vector< reco::TrackRef > > > vvvr1;        
+        edm::Wrapper<std::vector<std::vector<std::vector< reco::TrackRef > > > > wvvvr1;
         
         SelectedKinematicDecay d1;
         std::vector< SelectedKinematicDecay > vd1;
