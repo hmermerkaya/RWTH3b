4b2169539fe72a71f7369c16226014b4ad924712
perchall <>
2012-01-31 17:26:26 +0000

    adapt name changes

--- src/RecoTauTag/KinematicTau/python/KinematicTauSkim_cfi.py
+++ src/RecoTauTag/KinematicTau/python/KinematicTauSkim_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 KinematicTauSkim = cms.EDFilter("KinematicTauSkim",
-	kinematicTaus =	cms.InputTag("KinematicTauBasicProducer"),
+	kinematicTaus =	cms.InputTag("KinematicTauProducer"),
 	#discriminators = cms.vstring("PFRecoTauDiscriminationByKinematicFit","PFRecoTauDiscriminationByKinematicFitQuality")
 	discriminators = cms.vstring("PFRecoTauDiscriminationByKinematicFitQuality"),
 	minTau = cms.untracked.uint32(1)#minimum taus to produce (otherwise filter returns false)	
