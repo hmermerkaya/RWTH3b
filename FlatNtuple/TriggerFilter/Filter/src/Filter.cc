fee0ef9e0b4beb839ef34c1241ecc3cc99cb739f
cherepan <>
2012-02-23 15:24:29 +0000

    evaluate possible HLTpath changes

--- FlatNtuple/TriggerFilter/Filter/src/Filter.cc
+++ FlatNtuple/TriggerFilter/Filter/src/Filter.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Vladimir Cherepanov
 //         Created:  Wed Nov 23 16:24:12 CET 2011
-// $Id$
+// $Id: Filter.cc,v 1.1 2011/12/18 15:12:01 cherepan Exp $
 //
 //
 
@@ -99,16 +99,21 @@ Filter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
   bool accept = false;
+  bool Muon_Tau = false;
+  bool Muon_Eta_Tau = false;
+  bool Muon_Tau_accept = false;
+  bool Muon_Eta_Tau_accept = false;
+
   iEvent_ = &iEvent;
   cnt_++;
-
-//   char TrigerTemplate[28];
-//   sprintf(TrigerTemplate,"HLT_IsoMu%d_LooseIsoPFTau%d",15,15);
-
-
-
+  
+  //   char TrigerTemplate[28];
+  //   sprintf(TrigerTemplate,"HLT_IsoMu%d_LooseIsoPFTau%d",15,15);
+  
+  
+  
   edm::InputTag trigResultsTag("TriggerResults","","HLT");
- 
+  
   
   edm::Handle<edm::TriggerResults > TriggerResults_;
   iEvent_->getByLabel(trigResultsTag, TriggerResults_);
@@ -117,68 +122,86 @@ Filter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   const edm::TriggerNames& trigNames = iEvent.triggerNames(*TriggerResults_);   
   std::string passedTriggerName;
   if (TriggerResults_.isValid()) {
-
-    std::string TrigerTemplateDATA_1 = "HLT_IsoMu12_LooseIsoPFTau10";  // reco-1
-    std::string TrigerTemplateDATA_2 = "HLT_IsoMu15_LooseIsoPFTau15";  // reco-1
-
-    std::string TrigerTemplateDATA_6 = "HLT_IsoMu15_LooseIsoPFTau15";  // reco-6
-    std::string TrigerTemplateMC = "HLT_IsoMu15_LooseIsoPFTau15";
+    std::string checkMuon = "HLT_IsoMu";
+    std::string checkTau = "_LooseIsoPFTau";
+    std::string checkEta = "eta2p1";
 
 
 
 
-    std::string temp1 = "HLT_IsoMu";
-    std::string temp2 = "_LooseIsoPFTau";
-
+    //-----------------  Triggers to be checked ----------------------    
+    //HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v1
+    //HLT_IsoMu15_LooseIsoPFTau15_v9
+    //---------------------------------------------------------------
+    //if both triggers found the result of
+    //HLT_IsoMu15_eta2p1_LooseIsoPFTau20_v1  is taken
+    //
+    //----------------------------------------------------------------
     int ntrigs=TriggerResults_->size();
     for (int itrig = 0; itrig < ntrigs; ++itrig) {
       //std::cout << trigNames.triggerName(itrig) << std::endl;
       
+      std::string Muon;
+      std::string Tau;
+      std::string Eta;
       
-      std::string str1;
-      std::string str2;
-
-      str1 = trigNames.triggerName(itrig).substr(0,9);
-      //   std::cout<<"size " <<trigNames.triggerName(itrig).size() <<std::endl;
-      if(str1 == temp1 && trigNames.triggerName(itrig).size() > 20){
-
-      str2 = trigNames.triggerName(itrig).substr(11,14);
-
-      //   std::cout<<str1 <<"  "<<str2 <<std::endl;
-
-      if(str2 == temp2){
-	std::cout<<"Coincide: " <<str1 <<"   "<<str2 <<"  " <<trigNames.triggerName(itrig) <<std::endl;
- 	accept = (*TriggerResults_).accept(itrig);
- 	passedTriggerName = trigNames.triggerName(itrig);
+      Muon = trigNames.triggerName(itrig).substr(0,9);
+      // std::cout<<"size " <<trigNames.triggerName(itrig).size() <<std::endl;
+      if(Muon == checkMuon && trigNames.triggerName(itrig).size() > 20){
 	
-
-//       if(trigNames.triggerName(itrig)  == "HLT_IsoMu15_LooseIsoPFTau15_v8"){ 
-// 	// HLT_IsoMu15_LooseIsoPFTau15_v8
-// 	// HLT_IsoMu15_LooseIsoPFTau20_v6
-// 	// HLT_IsoMu15_TightIsoPFTau20_v6
-// 	accept = (*TriggerResults_).accept(itrig);
-// 	passedTriggerName = trigNames.triggerName(itrig);
-//       }
-      }
+	Tau = trigNames.triggerName(itrig).substr(11,14);
+	Eta = trigNames.triggerName(itrig).substr(12,6);
+	//std::cout<<Muon <<"  "<<Tau <<"  "<<Eta<<std::endl;
+	if(Tau == checkTau){
+	  //std::cout<<"HLT_IsoMu15_LooseIsoPFTau15  FOUND!  " <<Muon <<"   "<<Tau <<"  " <<trigNames.triggerName(itrig) <<std::endl;
+	  Muon_Tau = true;
+	  Muon_Tau_accept = (*TriggerResults_).accept(itrig);
+	  passedTriggerName = trigNames.triggerName(itrig);
+	  
+	}else if(Eta == checkEta){
+	  //std::cout<<"HLT_IsoMu15_LooseIsoPFTau15  Not  FOUND!  " <<std::endl;
+	  Tau =  trigNames.triggerName(itrig).substr(18,14);
+	  if(Tau == checkTau){
+	    Muon_Eta_Tau = true;
+	    Muon_Eta_Tau_accept = (*TriggerResults_).accept(itrig);
+	    passedTriggerName = trigNames.triggerName(itrig);
+	  }
+	}
       }
     }
     
   }else{
     std::cout<< "WARNING! Trigger results is not valid! "<<std::endl;
   }
-  
+  if(Muon_Tau && !Muon_Eta_Tau){
+    accept = Muon_Tau_accept;
+    // std::cout<<"Only first trigger found; Return its result  " << Muon_Tau_accept <<std::endl;
+
+  }
+  if(!Muon_Tau && Muon_Eta_Tau){
+
+    accept = Muon_Eta_Tau_accept;
+    //  std::cout<<"Only second trigger found; Return its result  " << Muon_Eta_Tau_accept <<std::endl;
+
+  }
+  if(Muon_Tau && Muon_Eta_Tau){
+
+    accept = Muon_Eta_Tau_accept;
+    //  std::cout<<"Both triggers found; Return result of the second  " << Muon_Tau_accept << " <>  "<< Muon_Eta_Tau_accept <<std::endl;
+
+  }
+
+  if(!Muon_Tau && !Muon_Eta_Tau){
+
+    accept = false;
+    //std::cout<<"No triggers found; Return FALSE" <<std::endl;
+
+  }
+
   if(accept) cntFound_++;
-		
-
-
-
-
-
-
-   return accept;
-
-
-
+  
+  return accept;
+  
 }
 
 // ------------ method called once each job just before starting event loop  ------------
