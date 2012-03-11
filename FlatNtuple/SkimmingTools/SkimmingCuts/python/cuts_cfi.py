6de8f398687d74c3ed7b8de6ca623d83faef5828
cherepan <>
2012-03-11 21:31:21 +0000

    cuts_cfi.py

--- FlatNtuple/SkimmingTools/SkimmingCuts/python/cuts_cfi.py
+++ FlatNtuple/SkimmingTools/SkimmingCuts/python/cuts_cfi.py
@@ -2,10 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 PreselectionCuts = cms.EDFilter('SkimmingCuts',
                     hpsTauProducer=cms.InputTag("hpsPFTauProducer"),
-                    MuonPtCut = cms.double(17.0),
+                    MuonPtCut = cms.double(24.0),
                     MuonIsGlobal = cms.bool(True),
+                    NMuons      = cms.double(1.0),
                     MuonEtaCut = cms.double(2.1),
-                    PFTauPtCut =cms.double(17.0),
+                    PFTauPtCut =cms.double(15.0),
                     PFTauEtaCut=cms.double(2.1)
 
 )
