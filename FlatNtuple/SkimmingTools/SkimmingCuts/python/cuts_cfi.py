f2ac90594d144d54813ae2dbef599cf5469db9b5
inugent <>
2013-05-22 20:04:55 +0000

    New skimming flag for muon

--- FlatNtuple/SkimmingTools/SkimmingCuts/python/cuts_cfi.py
+++ FlatNtuple/SkimmingTools/SkimmingCuts/python/cuts_cfi.py
@@ -1,18 +1,33 @@
 import FWCore.ParameterSet.Config as cms
 
 PreselectionCuts = cms.EDFilter('SkimmingCuts',
-                    hpsTauProducer=cms.InputTag("hpsPFTauProducer"),
-                    kinematicTausAdvanced = cms.InputTag("KinematicTauProducer","KinematicFitTau"),                                
-                    MuonPtCut = cms.double(18.0),
-                    MuonIsGlobal = cms.bool(True),
-                    ElectronPtCut = cms.double(20.0),
-                    ElectronEtaCut = cms.double(2.1),
-                    NMuons      = cms.double(1.0),
-                    MuonEtaCut = cms.double(2.1),
-                    PFTauPtCut =cms.double(18.0),
-                    PFTauEtaCut=cms.double(2.1)
-
+                                hpsTauProducer=cms.InputTag("hpsPFTauProducer"),
+                                kinematicTausAdvanced = cms.InputTag("KinematicTauProducer","KinematicFitTau"),                                
+                                MuonPtCut = cms.double(18.0),
+                                MuonIsGlobal = cms.bool(True),
+                                ElectronPtCut = cms.double(20.0),
+                                ElectronEtaCut = cms.double(2.1),
+                                NMuons      = cms.double(1.0),
+                                MuonEtaCut = cms.double(2.1),
+                                PFTauPtCut =cms.double(18.0),
+                                PFTauEtaCut=cms.double(2.1),
+                                doMuonOnly=cms.bool(False)
 )
+
+MuonPreselectionCuts = cms.EDFilter('SkimmingCuts',
+                                    hpsTauProducer=cms.InputTag("hpsPFTauProducer"),
+                                    kinematicTausAdvanced = cms.InputTag("KinematicTauProducer","KinematicFitTau"),
+                                    MuonPtCut = cms.double(18.0),
+                                    MuonIsGlobal = cms.bool(True),
+                                    ElectronPtCut = cms.double(20.0),
+                                    ElectronEtaCut = cms.double(2.1),
+                                    NMuons      = cms.double(1.0),
+                                    MuonEtaCut = cms.double(2.1),
+                                    PFTauPtCut =cms.double(18.0),
+                                    PFTauEtaCut=cms.double(2.1),
+                                    doMuonOnly=cms.bool(True)
+                                    )
+
 
 
 ControlSample_Skim = cms.EDFilter('ControlSample_SkimCuts',
